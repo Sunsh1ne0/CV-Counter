8bf9863530570b8420059ecf83faf0dc291a3336
Alexey Gusarov <a.gusarov@agrobit.ru>
2023-12-15 16:50:33 +0300

    fixed counting

--- Main.py
+++ Main.py
@@ -39,7 +39,7 @@ def draw_enter_end_zones(cv_image,horizontal=False):
             cv_image = cv2.line(cv_image, (int(width*end_zone_part), 0), (int(width*end_zone_part), height), (0,0,255), 1)
         return cv_image
 
-track_history = defaultdict(lambda: [[], False])
+track_history = defaultdict(lambda: [[], False, 0])
 count = 0
 
 def is_track_pass_board(track, horizontal = False):
@@ -91,16 +91,38 @@ def update_tracks(results, horizontal = False):
         track_C = track_history[track_id]
         track = track_C[0]
         is_counted = track_C[1]
-        track.append((float(x), float(y)))  # x, y center point
+        track.append((float(x),float(y)))
         if track[-1][index] > criteria * end_zone_part and is_counted == False:
             if is_track_pass_board(track, horizontal = horizontal):
                 count += 1
-                del track_history[track_id]
-
+                track_C[1] = True
+  
+    ## clear lost
+    keys = list(track_history.keys())
+    for key in keys:
+        if not key in track_ids:
+            if( track_history[key][2] < 30):
+                track_history[key][2] += 1
+            else:
+                del track_history[key]
+            
+        
 def draw_count(cv_image):
     return cv2.putText(cv_image, f"{count}", (30,30), 
-                       cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 
                        2, cv2.LINE_AA)
+
+def draw_tracks(cv_image): 
+    for key,values in track_history.items(): 
+        color = (0, 0, 255)
+        position = values[0][-1]
+        status = values[1]
+        if (status):
+            color = (0, 255, 0)
+        cv_image = cv2.circle(cv_image, (int(position[0]), int(position[1])), 0, color, 3) 
+    return cv_image
+        
+        
 
 def runserver():
     flask_server.app.run(debug=False, host="0.0.0.0")
@@ -137,7 +159,7 @@ while True:
         with flask_server.lock: 
             results = model.track(frame, persist=True, imgsz=128, tracker="tracker.yaml",verbose=False)
             update_tracks(results,horizontal = horizontal)
-            annotated_frame = results[0].plot(labels=False)
+            annotated_frame = draw_tracks(frame)
             annotated_frame = draw_enter_end_zones(annotated_frame, horizontal = horizontal)
             annotated_frame = draw_count(annotated_frame)
 
