3437a79c771c87dc491042399b6d313cc07cd6fb
Alexey Gusarov <a.gusarov@agrobit.ru>
2024-02-22 13:41:38 +0300

    undelivered data sending

--- TelemetryServer.py
+++ TelemetryServer.py
@@ -23,11 +23,28 @@ class TelemetryServer():
             channel.basic_publish(exchange='clickhouse-exchange',
                                  routing_key = self.queue,
                                  body = message)
+
             connection.close()
             return True
         except Exception as e:
             print("AMQP Connection Error:", e)     
             return False
+
+    def send_undelivered(self):
+        rows = localDB.undelivered()
+        for row in rows:
+            datetime = int(row[0])
+            value = row[1]
+            msg_string = f"{{\"timestamp\":{datetime}," + \
+                       f"\"FarmId\" :\"{self.FarmId}\"," + \
+                       f"\"LineId\" : {self.LineId}," + \
+                       "\"metric\":\"count\"," + \
+                       f"\"value\":{value}}}\n" 
+            success = self.publish_message(msg_string) 
+            if(success == True):
+                localDB.updateStatus(row[0])
+            
+        
 
     def send_count(self, N, datetime ):
         #prepare string for sending and saves to local DB sqlite3
@@ -37,7 +54,9 @@ class TelemetryServer():
                    f"\"LineId\" : {self.LineId}," + \
                    "\"metric\":\"count\"," + \
                    f"\"value\":{N}}}\n" 
-
-        success = self.publish_message(msg_string)
+        #if message delivered success col in local db set 1 else 0
+        success = self.publish_message(msg_string) 
         localDB.insert(datetime, N, success)  
+        if(success == True):
+            self.send_undelivered()
         return success
