0995b796b55e12bb571d41e1bc23dd2a402fa32a
SERGEI BAKULIN <68145047+Sunsh1ne0@users.noreply.github.com>
2024-06-28 13:49:57 +0300

    Poetry cfg added

---
+++ requirements_installation.sh
@@ -0,0 +1,14 @@
+sudo apt update && sudo apt upgrade
+
+sudo apt install cmake
+
+pip install --upgrade pip
+pip install poetry
+
+sudo apt install -y libkms++-dev libfmt-dev libdrm-dev
+pip install rpi-kms
+
+sudo apt install -y libcamera-dev
+pip install rpi-libcamera
+
+poetry install
